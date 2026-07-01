3fcf54fce4d085dc578e220a71f2e85ad24cf83d
Krzysztof <jujujj2007@gmail.com>
2026-07-01 21:21:32 +0200

    implement the websocket serwer

--- src/routes/matches.js
+++ src/routes/matches.js
@@ -50,6 +50,10 @@ matchRouter.post("/", async (req, res) => {
       })
       .returning()
 
+      if(res.app.locals.broadcastMatchCreated) {
+        res.app.locals.broadcastMatchCreated(event)
+      }
+
     res.status(201).json({ data: event })
   } catch (error) {
     res.status(500).json({ error: "Failed to create match.", details: JSON.stringify(error) })
